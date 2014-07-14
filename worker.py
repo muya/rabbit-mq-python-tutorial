fd0cd0fe1213fd1c681948e2b95387a0a0353487
Muya <kingkonig@gmail.com>
2014-07-14 10:19:17 +0300

    completed section 2
    - learnt how to make sure RabbitMQ doesn't forget queues and messages
      when it quits or crashes
    - learnt how to do a fair dispatch

--- worker.py
+++ worker.py
@@ -5,7 +5,7 @@ connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='task_queue', durable=True)
 
 
 def callback(ch, method, properties, body):
@@ -14,6 +14,7 @@ def callback(ch, method, properties, body):
     print " [x] Done!"
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
-channel.basic_consume(callback, queue='hello')
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(callback, queue='task_queue')
 
 channel.start_consuming()
